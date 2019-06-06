f9ac21c6d3d63fcde5cca4b286903768711584e7
TheStanster <48187702+TheStanster@users.noreply.github.com>
2019-06-06 14:01:07 +0200

    v0.02

--- TicTacToePython.py
+++ TicTacToePython.py
@@ -1,20 +1,49 @@
-
+# TODO Write in this comment what this file represents in the scope of the entire project
 
 
 def intro():
-	print("    SSS   V     V")
-	print("  S    S  V     V  7 | 8 | 9")
-	print("  S       V     V -----------")
-	print("    SSS   V     V  4 | 5 | 6")
-	print("       S  V     V -----------")
-	print("  S    S   V   V   1 | 2 | 3")
-	print("    SSS      V   ")
+    print("    SSS   V     V")
+    print("  S    S  V     V  7 | 8 | 9")
+    print("  S       V     V -----------")
+    print("    SSS   V     V  4 | 5 | 6")
+    print("       S  V     V -----------")
+    print("  S    S   V   V   1 | 2 | 3")
+    print("    SSS      V   ")
+    print("\n")     #short game description
+    print("Welcome to SVTTT (Stan Valsters Tic-Tac-Toe), a game of shapes.")
+    print("\n")
+    print("Your goal is to beat the opposing user in getting three of your marks(x/o) in a row on the grid.") # TODO make this and the next line appear after a few moments
+    print("An example of the grid is shown above. The numbers represent the possible positions in which you can place your mark.\n")
 
+
+
+def playGame():
+
+    return
+
+
+def askToPlayGame():
+
+    return
+
+
+def askToPlayAgain():
+
+    return
+
+
+def presentGameSummary():
+
+    return
 
 
 def main():		# Entry point for application
-	
 	intro()
+    #askToPlayGame()
+    #playGame()
+    #presentGameSummary()
+    #askToPlayAgain()
+
 	return 0		# Exit application
 
 if __name__ == "__main__": main()

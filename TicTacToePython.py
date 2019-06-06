2336e88cadadb630cac2f9bd3d99c76138b1090a
TheStanster <48187702+TheStanster@users.noreply.github.com>
2019-06-06 14:07:08 +0200

    v0.025
    Fixed spelling error

--- TicTacToePython.py
+++ TicTacToePython.py
@@ -10,7 +10,7 @@ def intro():
     print("  S    S   V   V   1 | 2 | 3")
     print("    SSS      V   ")
     print("\n")     #short game description
-    print("Welcome to SVTTT (Stan Valsters Tic-Tac-Toe), a game of shapes.")
+    print("Welcome to SVTTT (Stan Valster's Tic-Tac-Toe), a game of shapes.")
     print("\n")
     print("Your goal is to beat the opposing user in getting three of your marks(x/o) in a row on the grid.") # TODO make this and the next line appear after a few moments
     print("An example of the grid is shown above. The numbers represent the possible positions in which you can place your mark.\n")

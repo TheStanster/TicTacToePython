9c97a73f8fd67312f85dce50cfaba0315969b26b
TheStanster <48187702+TheStanster@users.noreply.github.com>
2019-06-06 12:33:45 +0200

    Add files via upload
    The basis of the program containing the main method and user interaction code

---
+++ .gitignore/TicTacToePython.py
@@ -0,0 +1,20 @@
+
+
+
+def intro():
+	print("    SSS   V     V")
+	print("  S    S  V     V  7 | 8 | 9")
+	print("  S       V     V -----------")
+	print("    SSS   V     V  4 | 5 | 6")
+	print("       S  V     V -----------")
+	print("  S    S   V   V   1 | 2 | 3")
+	print("    SSS      V   ")
+
+
+
+def main():		# Entry point for application
+	
+	intro()
+	return 0		# Exit application
+
+if __name__ == "__main__": main()

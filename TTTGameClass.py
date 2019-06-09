721ed495a30c87a28136290af9c563cc2815c94e
TheStanster <48187702+TheStanster@users.noreply.github.com>
2019-06-09 16:07:13 +0200

    v0.3
    New:
    Game-loop complete, ready for testing

--- TTTGameClass.py
+++ TTTGameClass.py
@@ -1,16 +1,8 @@
 class TicTacToeGame():
     """Class that contains the game logic"""
-    
-    def Reset(self): # set game to initial state
-        self.bGameIsWon = False
-        self.bGameIsDraw = False
         
-        self.currentTurn = 0
-        self.currentMark = ''
-        self.playerNum = ''
-        
-    
-    def __init__(self): # class constructor
+    # class constructor
+    def __init__(self): 
         
         self.bGameIsWon = ''
         self.bGameIsDraw = ''
@@ -20,8 +12,17 @@ class TicTacToeGame():
         self.currentTurn = ''
         self.currentMark = ''
         self.playerNum = ''
+
+    # set game state variables to initial state
+    def Reset(self): 
+        self.bGameIsWon = False
+        self.bGameIsDraw = False
         
-     
+        self.currentTurn = 0
+        self.currentMark = ''
+        self.playerNum = ''
+        
+    # Checks if there is a draw or if game has been won  
     def BoardCheck(self, board, mark):
         if self.currentTurn == 9:
             self.bGameIsDraw = True
@@ -29,7 +30,7 @@ class TicTacToeGame():
         else: 
             self.bGameIsWon = self.IsGameWon(board, mark)
 
-
+    # Contains winconditions
     def IsGameWon(self, board, mark):
         return ((board[7] == mark and board[8] == mark and board[9] == mark) or # across the top
         (board[4] == mark and board[5] == mark and board[6] == mark) or # across the middle
@@ -40,13 +41,10 @@ class TicTacToeGame():
         (board[7] == mark and board[5] == mark and board[3] == mark) or # diagonal
         (board[9] == mark and board[5] == mark and board[1] == mark)) # diagonal
               
+    # Increments the turncount
+    def NextTurn(self): self.currentTurn += 1
 
-       
-
-        # Getters
-    def NextTurn(self):
-        self.currentTurn += 1
-
+    # Prints the game board in its current state
     def GetBoard(self,board):
         print('  ' + board[7] + '  |  ' +  board[8] + '  |  ' + board[9] + '   ')
         print('-----------------')
@@ -55,21 +53,22 @@ class TicTacToeGame():
         print('  ' + board[1] + '  |  ' +  board[2] + '  |  ' + board[3] + '   ')
         print('\n')
     
+    # Determines the number of the player, whose turn it is to place a mark
+    def GetCurrentPlayerNumber(self):
+        if self.currentTurn % 2 == 1:
+            self.playerNum = '1'
+        else:
+            self.playerNum = '2'
+     
+    # Determines what mark belongs to the current player
     def GetCurrentPlayerMark(self):
         if self.currentTurn % 2 == 1:
             self.currentMark = self.userOneMark
         else:
             self.currentMark = self.userTwoMark
 
-    def GetCurrentPlayerNumber(self):
-        if self.currentTurn % 2 == 1:
-            self.playerNum = '1'
-        else:
-            self.playerNum = '2'
-
-
-    def place_mark(self, board, mark, position):
-        board[position] = mark
+    # Places a marker on the board
+    def place_mark(self, board, mark, position): board[position] = mark
 
 
        
--- TicTacToePython.py
+++ TicTacToePython.py
@@ -2,8 +2,9 @@
 
 import TTTGameClass # imports the game class
 
-
-TTTGame = TTTGameClass.TicTacToeGame() # New object of the game class
+# New object of the game class
+TTTGame = TTTGameClass.TicTacToeGame() 
+# Create an empty board
 board = [' '] * 10
 
 # Prints logo, example grid and a short game description
@@ -21,7 +22,7 @@ def intro():
     print("Your goal is to beat the opposing user in getting three of your marks(x/o) in a row on the board.") # TODO make this and the next line appear after a few moments
     print("An example of the board is shown above. The numbers represent the possible positions in which you can place your mark.\n")                        
 
-# Starts a single round of Tic-Tac-Toe
+# Starts a single round of Tic-Tac-Toe and runs until the round is finished
 def playGame():
     
     TTTGame.Reset()
@@ -35,6 +36,7 @@ def playGame():
 
     return   
 
+# Lets the users take turns until the game is either won or ends in a draw
 def takeTurns():
     while TTTGame.bGameIsWon == False and TTTGame.bGameIsDraw == False:
         TTTGame.NextTurn()
@@ -44,70 +46,69 @@ def takeTurns():
         TTTGame.GetBoard(board)
         TTTGame.BoardCheck(board, TTTGame.currentMark)
         
-
+# Asks for user input until a valid value is supplied, then submits it to the board
 def submitChoice():
     TTTGame.GetCurrentPlayerMark()
     TTTGame.GetCurrentPlayerNumber()
 
-    choice = input("Player " + TTTGame.playerNum + " please enter the number corresponding to the position on the board where you want to place your mark. \n")
-    choice = int(choice)
-    if board[choice] == TTTGame.userOneMark or board[choice] == TTTGame.userTwoMark:
-        print("This position has already been chosen. Please enter an empty position on the board. \n")
-        return submitChoice() 
-    elif choice >= 1 and choice <= 9:
-        TTTGame.place_mark(board, TTTGame.currentMark, choice)
-    else:
+    try:    # makes sure input is an integer
+        choice = input("Player " + TTTGame.playerNum + " please enter the number corresponding to the position on the board where you want to place your mark. \n")
+        choice = int(choice)
+        if board[choice] == TTTGame.userOneMark or board[choice] == TTTGame.userTwoMark:
+            print("This position has already been chosen. Please enter an empty position on the board. \n")
+            return submitChoice() 
+        elif choice >= 1 and choice <= 9:
+            TTTGame.place_mark(board, TTTGame.currentMark, choice)
+        else:   #To catch those pesky zeros
+            print("Please enter a number between 1 and 9. \n")
+            return submitChoice()
+    except:
         print("Please enter a number between 1 and 9. \n")
         return submitChoice()
 
-
 # Ask if user wants to play before start of first game
 def askToPlayGame():
-
-    answer = ''
-    while (answer == ''):
-        answer = input("Would you like to play a game of tic-tac-toe? (y/n) \n")
+        
+    answer = input("Would you like to play a game of tic-tac-toe? (y/n) \n").lower()
+    print('\n')
+    if (answer == 'yes' or answer == 'y'):
+        print("Player 1 will be represented on the board by an " + TTTGame.userOneMark + " mark.")
+        print("Player 2 will be represented on the board by an " + TTTGame.userTwoMark + " mark. \n")
+        return 
+    elif (answer == 'no' or answer == 'n'):
+        answer = ''
+        answer = input("Are you sure? (y/n) \n").lower()
         print('\n')
         if (answer == 'yes' or answer == 'y'):
-            print("Player 1 will be represented on the board by an " + TTTGame.userOneMark + " mark.")
-            print("Player 2 will be represented on the board by an " + TTTGame.userTwoMark + " mark. \n")
-            return # TODO check if this loop is having errors by there being no break statements 
-        elif (answer == 'no' or answer == 'n'):
-            answer = ''
-            answer = input("Are you sure? (y/n) \n")
-            print('\n')
-            if (answer == 'yes' or answer == 'y'):
-                print("Have a nice day!")
-                return exit ()
-            else:
-                return askToPlayGame()
+            print("Have a nice day!")
+            return exit ()
         else:
-            print("I didn't quite get that.\n")
-            return askToPlayGame()               
-
+            return askToPlayGame()
+    else:
+        print("I didn't quite get that.\n")
+        return askToPlayGame()
+               
 # Ask whether the user wants to play again when a round has finished
 def askToPlayAgain():
     
-    answer = ''
-    while (answer == ''):
-        answer = input("Would you like to play another round? (y/n) \n")
+    answer = input("Would you like to play another round? (y/n) \n").lower()
+    print('\n')
+    if (answer == 'yes' or answer == 'y'):
+        return playGame() 
+    elif (answer == 'no' or answer == 'n'):
+        answer = ''
+        answer = input("Are you sure? (y/n) \n").lower()
         print('\n')
         if (answer == 'yes' or answer == 'y'):
-            return playGame() 
-        elif (answer == 'no' or answer == 'n'):
-            answer = ''
-            answer = input("Are you sure? (y/n) \n")
-            print('\n')
-            if (answer == 'yes' or answer == 'y'):
-                print("Have a nice day!")
-                return exit ()
-            else:
-                return askToPlayAgain()
+            print("Have a nice day!")
+            return exit ()
         else:
-            print("I didn't quite get that.\n")
             return askToPlayAgain()
-    return               
-
+    else:
+        print("I didn't quite get that.\n")
+        return askToPlayAgain()
+              
+# Clears the board, so that values entered onto the board do not carry over to succesive rounds
 def ResetBoard():
     board[1]= ' '
     board[2]= ' '
@@ -119,10 +120,19 @@ def ResetBoard():
     board[8]= ' '
     board[9]= ' '
     
-
+# Informs the user that the round is finished 
+# Also presents whether there was a draw or a win, and if applicable, who won in how many turns
 def presentGameSummary():
-
-    return
+    if TTTGame.bGameIsDraw == True:
+        print("That's a draw!")
+        print("It seems that you were evenly matched.")
+    elif TTTGame.bGameIsWon == True:
+        print("Congratulations!")
+        print("Player " + TTTGame.playerNum + " managed to beat their opponent in " + str(TTTGame.currentTurn) + " turns.")
+        print("\n")
+    else:
+        print("The game was was neither lost nor was it a draw.")
+        print("How did you even get here?")
 
 # Entry point for application
 def main():		
